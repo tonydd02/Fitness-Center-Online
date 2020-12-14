0b6c26067ac3564e829c739ffeedd2d54446c43c
ZHDphilip <zihaodong@ucla.edu>
2020-12-14 14:14:52 +0800

    Update dependency for styling and added planning exercise feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import { Provider } from 'next-auth/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react'
 
 export default function App ({ Component, pageProps }) {
+  useEffect(() => {
+    import("jquery").then($ => {
+      // jQuery must be installed to the `window`:
+      window.$ = window.jQuery = $;
+      return import("bootstrap");
+    });
+  }, []);
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
